80f3f68aa5eb6a4880088c711fb99105bfc18a36
HabibAroua <habibha.aroua82@gmail.com>
2019-08-24 11:29:47 +0100

    fix the project

---
+++ app_mobile/app/src/main/java/com/example/app_mobile/route/Route.java
@@ -0,0 +1,9 @@
+package com.example.app_mobile.route;
+
+public class Route
+{
+    private static final String PROTOCOL="http://";
+    private static final String ADDRESS="192.168.1.8";
+    public  static final String URL=PROTOCOL+ADDRESS+"/webservice/Public/";
+    public  static final String URL_LOGIN=URL+"index.php";
+}
