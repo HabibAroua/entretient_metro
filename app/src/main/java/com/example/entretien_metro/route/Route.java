00f89020fc7b737c8b40ab094176af0a717518cd
HabibAroua <habibha.aroua82@gmail.com>
2019-08-24 11:40:27 +0100

    fix the project

--- app/src/main/java/com/example/entretien_metro/route/Route.java
+++
@@ -1,9 +0,0 @@
-package com.example.entretien_metro.route;
-
-public class Route
-{
-    private static final String PROTOCOL="http://";
-    private static final String ADDRESS="192.168.1.8";
-    public  static final String URL=PROTOCOL+ADDRESS;
-    public  static final String URL_LOGIN=URL+"/webservice";
-}
