d51c352d8297587923665569ca114458ab2e34f9
Habib Aroua <habib.aroua@sesame.com.tn>
2019-12-16 23:07:54 +0100

    Update GMailSender.java

--- app_mobile/app/src/main/java/com/example/app_mobile/mailing/GMailSender.java
+++ app_mobile/app/src/main/java/com/example/app_mobile/mailing/GMailSender.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.Security;
 import java.util.Properties;
-
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.mail.Message;
@@ -16,39 +15,41 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class GMailSender extends javax.mail.Authenticator {
+public class GMailSender extends javax.mail.Authenticator
+{
     private String mailhost = "smtp.gmail.com";
     private String user;
     private String password;
     private Session session;
 
-    static {
+    static 
+    {
         Security.addProvider(new JSSEProvider());
     }
 
-    public GMailSender(String user, String password) {
+    public GMailSender(String user, String password) 
+    {
         this.user = user;
         this.password = password;
-
         Properties props = new Properties();
         props.setProperty("mail.transport.protocol", "smtp");
         props.setProperty("mail.host", mailhost);
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "465");
         props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.socketFactory.fallback", "false");
         props.setProperty("mail.smtp.quitwait", "false");
-
         session = Session.getDefaultInstance(props, this);
     }
 
-    protected PasswordAuthentication getPasswordAuthentication() {
+    protected PasswordAuthentication getPasswordAuthentication() 
+    {
         return new PasswordAuthentication(user, password);
     }
 
-    public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception {
+    public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception 
+    {
         try
         {
             MimeMessage message = new MimeMessage(session);
@@ -57,9 +58,13 @@ public class GMailSender extends javax.mail.Authenticator {
             message.setSubject(subject);
             message.setDataHandler(handler);
             if (recipients.indexOf(',') > 0)
+            {
                 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
+            }
             else
+            {
                 message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
+            }
             Transport.send(message);
         }
         catch(Exception e)
@@ -68,41 +73,52 @@ public class GMailSender extends javax.mail.Authenticator {
         }
     }
 
-    public class ByteArrayDataSource implements DataSource {
+    public class ByteArrayDataSource implements DataSource 
+    {
         private byte[] data;
         private String type;
-
-        public ByteArrayDataSource(byte[] data, String type) {
+        public ByteArrayDataSource(byte[] data, String type) 
+        {
             super();
             this.data = data;
             this.type = type;
         }
 
-        public ByteArrayDataSource(byte[] data) {
+        public ByteArrayDataSource(byte[] data)
+        {
             super();
             this.data = data;
         }
 
-        public void setType(String type) {
+        public void setType(String type) 
+        {
             this.type = type;
         }
 
-        public String getContentType() {
+        public String getContentType()
+        {
             if (type == null)
+            {
                 return "application/octet-stream";
+            }
             else
+            {
                 return type;
+            }
         }
 
-        public InputStream getInputStream() throws IOException {
+        public InputStream getInputStream() throws IOException 
+        {
             return new ByteArrayInputStream(data);
         }
 
-        public String getName() {
+        public String getName() 
+        {
             return "ByteArrayDataSource";
         }
 
-        public OutputStream getOutputStream() throws IOException {
+        public OutputStream getOutputStream() throws IOException 
+        {
             throw new IOException("Not Supported");
         }
     }
